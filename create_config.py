99451ec14db47734685ad6a92f8237132ba7fcba
ScottY <scotty@scottm3air.attlocal.net>
2024-05-16 00:15:15 -0700

    test

--- create_config.py
+++ create_config.py
@@ -1,5 +1,19 @@
-import yaml
+import random
 
+import requests
+import yaml
+from flask import Flask, render_template, request
+import sys
+import os
+import subprocess
+import time
+
+processed = {
+
+}
+
+
+app = Flask(__name__)
 
 data = {
     "base": "",
@@ -13,3 +27,36 @@ data = {
     }
 
 }
+base_root = sys.argv[1]
+config_root=f"{base_root}/config/"
+img_root = f"{base_root}/img/"
+result_root=f"{base_root}/result/"
+
+print("Using base root=", base_root)
+
+def list_config_yaml(directory):
+    yaml_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.yaml'):
+                yaml_files.append(os.path.join(root, file))
+    return yaml_files
+
+
+@app.route("/")
+def root():
+    return render_template("root.html", configs = list_config_yaml(config_root))
+@app.route("/run", methods=['POST'])
+def run_config():
+    config_2_run = request.form['config']
+    print("To run", config_2_run)
+    key = f"{time.time()}"
+    try:
+        cmd = ["python3" , config_2_run]
+        print(cmd)
+        output = subprocess.check_output(cmd)
+    except subprocess.SubprocessError as e:
+        return f"ERROR: {e}\r\n {e.output}"
+    return f"OK :{output}"
+if __name__=="__main__":
+    app.run(debug=True, port=8199)
